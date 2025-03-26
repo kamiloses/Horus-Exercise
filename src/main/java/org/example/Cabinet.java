29b7408290e746385063f9b419a5e0dea83a5aa0
kamil <kamil.kurzaj04@gmail.com>
2025-03-26 11:38:08 +0100

    initial code

---
+++ src/main/java/org/example/Cabinet.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+import java.util.Optional;
+
+interface Cabinet {
+    Optional<Folder>
+    findFolderByName(String name);
+
+    List<Folder> findFoldersBySize(String size);
+
+    int count();
+}
---
+++ src/main/java/org/example/FileCabinet.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.List;
+
+public class FileCabinet implements Cabinet {
+private List<Folder> folders;
+}
